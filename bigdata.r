0669eb93538a16dbfe14f8a6c15dd1e6e9492ed8
sehnryr <youn@melois.dev>
2023-06-12 11:23:02 +0200

    convert "NULL" to NA in R

--- bigdata.r
+++ bigdata.r
@@ -1,6 +1,9 @@
 # Read stat_acc_V3.csv for data preparation
 data <- read.csv("stat_acc_V3.csv", header = TRUE, sep = ";")
 
+# Convert "Null" values to NA
+data[data == "NULL"] <- NA
+
 # Convert the date column to date format
 data$date <- as.Date(data$date, format = "%Y-%m-%d %H:%M:%S")
 
