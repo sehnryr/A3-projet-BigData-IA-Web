d2ae1b8cae0db06367001e78f21626b5d4b89023
sehnryr <youn@melois.dev>
2023-06-12 11:06:05 +0200

    create basic R script for the data preparation + added columns for the month and week number

---
+++ bigdata.r
@@ -0,0 +1,11 @@
+# Read stat_acc_V3.csv for data preparation
+data <- read.csv("stat_acc_V3.csv", header = TRUE, sep = ";")
+
+# Convert the date column to date format
+data$date <- as.Date(data$date, format = "%Y-%m-%d %H:%M:%S")
+
+# Add a month column to the data frame
+data$month <- as.numeric(format(data$date, "%m"))
+
+# Add a week column to the data frame
+data$week <- as.numeric(format(data$date, "%W"))
