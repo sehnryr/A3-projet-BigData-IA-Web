27313822f3abbed854d76700b249e853f012b149
AlexandreGirolt <alexandre.girolt@sfr.fr>
2023-06-13 10:49:44 +0200

    test

---
+++ analyse.r
@@ -0,0 +1,5 @@
+# Description: This script make chi2 independence tests between the different variables of the dataframe.
+
+# Load the preparation.r script to read and prepare data for use
+source("preparation.r")
+
