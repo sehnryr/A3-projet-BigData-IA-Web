785af78152593b9b64cae035892cd7b64c7ea8d2
sehnryr <youn@melois.dev>
2023-06-13 10:49:45 +0200

    add map visualization for the severe accident rates per region and department

--- visualisation.r
+++ visualisation.r
@@ -133,3 +133,37 @@ ggplot(france, aes(x=long, y=lat, group=group, fill=count)) +
   geom_polygon(colour="black") +
   coord_map("mercator") +
   scale_fill_gradient(low="blue",high="red")
+
+# Severe accidents rate per department
+data_severe <- data %>%
+  group_by(code_departement) %>%
+  summarise(count = n(), severe = sum(descr_grav == 3 | descr_grav == 4))
+
+data_severe$rate <- data_severe$severe / data_severe$count
+
+# Merge the data with the department names
+france$rate <- data_severe$rate[match(france$code_departement, data_severe$code_departement)]
+
+# Plot the map
+ggplot(france, aes(x=long, y=lat, group=group, fill=rate)) +
+  labs(title = "Severe accidents rate by department", x = "Longitude", y = "Latitude") +
+  geom_polygon(colour="black") +
+  coord_map("mercator") +
+  scale_fill_gradient(low="blue",high="red")
+
+# Severe accidents rate per region
+data_severe <- data %>%
+  group_by(code_region) %>%
+  summarise(count = n(), severe = sum(descr_grav == 3 | descr_grav == 4))
+
+data_severe$rate <- data_severe$severe / data_severe$count
+
+# Merge the data with the region names
+france$rate <- data_severe$rate[match(france$code_region, data_severe$code_region)]
+
+# Plot the map
+ggplot(france, aes(x=long, y=lat, group=group, fill=rate)) +
+  labs(title = "Severe accidents rate by region", x = "Longitude", y = "Latitude") +
+  geom_polygon(colour="black") +
+  coord_map("mercator") +
+  scale_fill_gradient(low="blue",high="red")
