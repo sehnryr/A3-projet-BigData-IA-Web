0b349f8139005fba70f8c558b85b1f2e98886ed4
sehnryr <youn@melois.dev>
2023-06-14 11:34:58 +0200

    fix label for some graphs

--- visualisation/graph_acc_atmo.r
+++ visualisation/graph_acc_atmo.r
@@ -3,14 +3,17 @@ data_atmospheric_condition <- data %>%
   group_by(descr_athmo) %>%
   summarise(count = n())
 
+# Add the label to the data
+data_atmospheric_condition$label <- names(valeur_descr_athmo)[
+  data_atmospheric_condition$descr_athmo]
+
 # Create the graph
-g <- ggplot(data_atmospheric_condition, aes(x = descr_athmo, y = count)) +
+g <- ggplot(data_atmospheric_condition, aes(x = reorder(label, descr_athmo), y = count)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_text(aes(label = count), vjust = -0.3, size = 3.5) +
   labs(title = "Accidents en fonction des conditions atmosphériques", x = "Condition atmosphérique", y = "Accidents") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_x_discrete(labels = setNames(names(valeur_descr_athmo), valeur_descr_athmo))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # Save the graph to png
 ggsave(g, file="export/graph_acc_atmo.png", width = 7, height = 7, dpi = 300)
--- visualisation/graph_acc_surf.r
+++ visualisation/graph_acc_surf.r
@@ -3,14 +3,17 @@ data_surface_condition <- data %>%
   group_by(descr_etat_surf) %>%
   summarise(count = n())
 
+# Add the label to the data
+data_surface_condition$label <- names(valeurs_descr_etat_surf)[
+  data_surface_condition$descr_etat_surf]
+
 # Create the graph
-g <- ggplot(data_surface_condition, aes(x = descr_etat_surf, y = count)) +
+g <- ggplot(data_surface_condition, aes(x = reorder(label, descr_etat_surf), y = count)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   geom_text(aes(label = count), vjust = -0.3, size = 3.5) +
   labs(title = "Accidents en fonction de la description de la surface", x = "Description de la surface", y = "Accidents") +
   theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  scale_x_discrete(labels = setNames(names(valeurs_descr_etat_surf), valeurs_descr_etat_surf))
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 # Save the graph to png
 ggsave(g, file="export/graph_acc_surf.png", width = 7, height = 7, dpi = 300)
