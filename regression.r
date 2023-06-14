ccaa6b5fa93e94d58c036c354661a7738d6128a7
sehnryr <youn@melois.dev>
2023-06-14 09:16:27 +0200

    first pass on linear regression

--- regression.r
+++ regression.r
@@ -1,8 +1,18 @@
+source("preparation.r")
+
 data_month <- data %>%
   group_by(month) %>%
   summarise(count = n())
 
-plot(data_month[month], data_month[count])
+lm_data_month <- lm(count ~ month, data = data_month)
+summary(lm_data_month)
+
+data_week <- data %>%
+  group_by(week) %>%
+  summarise(count = n())
+
+lm_data_week <- lm(count ~ week, data = data_week)
+summary(lm_data_week)
 
 # x <- c( 420, 380, 350, 400, 440, 380, 450, 420)   
 # y <- c( 5.5, 6, 6.5, 6, 5, 6.5, 4.5, 5)
