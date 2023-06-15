7def1cb67dea9e114cd82b35363276c327bedbb0
AlexandreGirolt <alexandre.girolt@sfr.fr>
2023-06-15 08:15:16 +0200

    regression fonctionelle

--- regression.r
+++ regression.r
@@ -5,17 +5,12 @@ data_month <- data %>%
   summarise(count = n())
 
 lm_data_month <- lm(count ~ month, data = data_month)
-summary(lm_data_month)
+print(summary(lm_data_month))
 
 data_week <- data %>%
   group_by(week) %>%
   summarise(count = n())
 
 lm_data_week <- lm(count ~ week, data = data_week)
-summary(lm_data_week)
+print(summary(lm_data_week))
 
-# x <- c( 420, 380, 350, 400, 440, 380, 450, 420)   
-# y <- c( 5.5, 6, 6.5, 6, 5, 6.5, 4.5, 5)
-
-
-# plot(x, y)
