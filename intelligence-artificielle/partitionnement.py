b206aedb2dbf370d25da00e537f134a132cfa66a
sehnryr <youn@melois.dev>
2023-06-19 15:10:49 +0200

    ajout du k-means avec sklearn

---
+++ intelligence-artificielle/partitionnement.py
@@ -0,0 +1,54 @@
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import normalized_mutual_info_score
+
+# Recupération du chemin du dossier courant
+# (le dossier contenant le fichier main.py)
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+# Lecture du fichier CSV exporté depuis le projet Big Data
+data = pd.read_csv(f"{current_path}/export_IA.csv")
+
+
+# Fonction de calcul des k-means à la main
+def k_means_scratch(X, n_clusters=4, n_init=10, max_iter=300, random_state=0):
+    pass
+
+
+# Fonction de calcul des k-means avec scikit-learn
+def k_means_sklearn(X, n_clusters=4, n_init=10, max_iter=300, random_state=0):
+    kmeans = KMeans(
+        n_clusters=n_clusters,
+        n_init=n_init,
+        max_iter=max_iter,
+        random_state=random_state,
+    ).fit(X)
+    indexes = kmeans.fit_predict(X)
+    return (kmeans, indexes)
+
+
+# Preparation des données pour le clustering
+# Recuperation des longitudes et latitudes par gravité d'accident
+X = data[["longitude", "latitude"]].values
+
+# On ne garde que les accidents qui se sont produits en France
+X = X[(X[:, 0] > -5) & (X[:, 0] < 10) & (X[:, 1] > 40) & (X[:, 1] < 52)]
+
+# On effectue le clustering
+kmeans, indexes = k_means_sklearn(X, n_clusters=4)
+
+# Graphique scatter des accidents sur une carte de France
+plt.figure(figsize=(10, 10))
+plt.title("Accidents en France")
+plt.xlabel("Longitude")
+plt.ylabel("Latitude")
+plt.scatter(X[:, 0], X[:, 1], c=indexes, s=2)
+plt.scatter(
+    kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c="red", s=100
+)
+plt.show()
+plt.close()
