43198adebdec34c8b3358c79b119718b4d3ab25c
sehnryr <youn@melois.dev>
2023-06-19 10:25:46 +0200

    affichage des informations de base du dataset

--- intelligence-artificielle/main.py
+++ intelligence-artificielle/main.py
@@ -3,3 +3,21 @@ import pandas as pd
 if __name__ == "__main__":
     # Lecture du fichier CSV exporté depuis le projet Big Data
     data = pd.read_csv("export_IA.csv")
+
+    # Les valeurs cible sont les valeurs de gravité d'un accident.
+    # Il y a 4 valeurs possibles : 1, 2, 3 et 4.
+    # 1 correspond à un accident sans gravité, 2 à un accident léger,
+    # 3 à un accident grave et 4 à un accident mortel.
+    print("Valeurs cible :", data["gravite"].unique())
+
+    # On affiche le nombre d'instances du dataset (nombre de lignes)
+    print("Nombre d'instances :", len(data))
+
+    # On affiche le nombre d'instances du dataset pour chaque valeur de gravité
+    print(
+        "Nombre d'instances pour chaque valeur de gravité :",
+        data["gravite"].value_counts().to_dict(),
+    )
+
+    # On affiche la taille des features (nombre de colonnes)
+    print("Nombre de features :", len(data.columns))
