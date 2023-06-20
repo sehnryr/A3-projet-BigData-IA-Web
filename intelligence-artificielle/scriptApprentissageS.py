b6297ddda85c11f9a34a69cd4bb38011e251c4e4
Alexandre-github-code <alexandre.auguste@isen-ouest.yncrea.fr>
2023-06-20 12:01:52 +0200

    mal compris la consigne, c cense marche mtn

---
+++ intelligence-artificielle/scriptApprentissageS.py
@@ -0,0 +1,34 @@
+import sys
+import numpy as np
+from sklearn.cluster import KMeans
+
+# Charger les coordonnées de latitude et de longitude de l'accident à partir des arguments en ligne de commande
+latitude = float(sys.argv[1])
+longitude = float(sys.argv[2])
+
+# Calculer le nombre de clusters
+num_clusters = (len(sys.argv) - 3) // 2
+
+# Charger les coordonnées des centroides des clusters à partir des arguments en ligne de commande
+centroids = []
+for i in range(num_clusters):
+    centroid_lat = float(sys.argv[i*2 + 3])
+    centroid_lon = float(sys.argv[i*2 + 4])
+    centroids.append([centroid_lat, centroid_lon])
+
+# Effectuer le clustering en utilisant l'algorithme K-means
+kmeans = KMeans(n_clusters=num_clusters, init=np.array(centroids))
+kmeans.fit([]) # car un seul accident
+
+# Prédire le cluster d'appartenance de l'accident
+accident = [[latitude, longitude]]
+cluster_label = kmeans.predict(accident)
+
+# Conversion du résultat en format JSON
+result = {'cluster_label': cluster_label}
+
+# Écriture du résultat au format JSON dans un fichier
+with open('result.json', 'w') as json_file:
+    json.dump(result, json_file)
+
+print("Résultat enregistré dans le fichier 'result.json'.")
