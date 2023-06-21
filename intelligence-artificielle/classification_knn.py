e65b3858948e15632aeb096bde596b7732d72867
sehnryr <youn@melois.dev>
2023-06-21 09:34:34 +0200

    maj de l'exemple de knn

--- intelligence-artificielle/classification_knn.py
+++ intelligence-artificielle/classification_knn.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-from statistics import mode
+from statistics import mode, mean
 
 from distance import *
 
@@ -48,6 +48,7 @@ def knn_sklearn(X_train, y_train, k=5):
 if __name__ == "__main__":
     from read import data
     from repartition import *
+    from time import time
 
     data = data[
         [
@@ -65,8 +66,27 @@ if __name__ == "__main__":
     # pour chaque label
     data = data.groupby("descr_grav").apply(lambda x: x.sample(frac=0.1))
 
+    # Stockage des scores pour chaque classifieur et chaque métrique pour
+    # faire des moyennes
+    scores = {
+        "sklearn": [],
+        "scratch-hamming": [],
+        "scratch-manhattan": [],
+        "scratch-euclidean": [],
+        "scratch-minkowski": [],
+    }
+
+    # Compte du nombre de folds
+    i = 0
+    time_start = time()
+
     # for data_train, data_test in repartition_leave_one_out_sklearn(data):
     for data_train, data_test in repartition_holdout_sklearn(data):
+        time_end = time()
+        print(f"Fold {i + 1} {round(time_end - time_start, 3)}s", end="\r")
+        time_start = time_end
+        i += 1
+
         X_train = data_train.drop(columns=["descr_grav"])
         y_train = data_train["descr_grav"]
 
@@ -75,22 +95,26 @@ if __name__ == "__main__":
 
         knn = knn_sklearn(X_train, y_train, k=15)
         knn_score = accuracy_score(y_test, knn.predict(X_test))
-        print("SciKit-Learn", round(knn_score, 4))
+        scores["sklearn"].append(knn_score)
 
         knn_hamming = knn_scratch(X_train, y_train, k=15, dist_metric=hamming_distance)
         knn_hamming_score = accuracy_score(y_test, knn_hamming.predict(X_test))
-        print("Scratch Hamming", round(knn_hamming_score, 4))
+        scores["scratch-hamming"].append(knn_hamming_score)
 
         knn_manhattan = knn_scratch(X_train, y_train, k=15, dist_metric=manhattan_distance)
         knn_manhattan_score = accuracy_score(y_test, knn_manhattan.predict(X_test))
-        print("Scratch Manhattan", round(knn_manhattan_score, 4))
+        scores["scratch-manhattan"].append(knn_manhattan_score)
 
         knn_euclidean = knn_scratch(X_train, y_train, k=15, dist_metric=euclidean_distance)
         knn_euclidean_score = accuracy_score(y_test, knn_euclidean.predict(X_test))
-        print("Scratch Euclidean", round(knn_euclidean_score, 4))
+        scores["scratch-euclidean"].append(knn_euclidean_score)
 
         knn_minkowski = knn_scratch(X_train, y_train, k=15, dist_metric=minkowski_distance)
         knn_minkowski_score = accuracy_score(y_test, knn_minkowski.predict(X_test))
-        print("Scratch Minkowski", round(knn_minkowski_score, 4))
+        scores["scratch-minkowski"].append(knn_minkowski_score)
 
-        break
+    print("SciKit-Learn:", round(mean(scores["sklearn"]), 4))
+    print("Scratch Hamming:", round(mean(scores["scratch-hamming"]), 4))
+    print("Scratch Manhattan:", round(mean(scores["scratch-manhattan"]), 4))
+    print("Scratch Euclidean:", round(mean(scores["scratch-euclidean"]), 4))
+    print("Scratch Minkowski:", round(mean(scores["scratch-minkowski"]), 4))
