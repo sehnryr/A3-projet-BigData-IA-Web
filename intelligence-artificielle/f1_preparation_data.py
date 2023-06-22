7ce70ccc967ca7f59299a9afe6c3c3f72baa4586
sehnryr <youn@melois.dev>
2023-06-22 10:53:43 +0200

    changement des longitudes et latitudes par le code_departement

--- intelligence-artificielle/f1_preparation_data.py
+++ intelligence-artificielle/f1_preparation_data.py
@@ -11,10 +11,15 @@ data = data.drop(
         "num_veh",
         "id_usa",
         "id_code_insee",
-        "code_departement",
         "ville",
     ]
 )
+
+# Remplacement des valeurs de departement 2A et 2B par 100 et 101
+data["code_departement"] = data["code_departement"].replace({"2A": 100, "2B": 101})
+
+# Conversion de la colonne code_departement en int
+data["code_departement"] = data["code_departement"].astype(int)
 
 # Conversion de la colonne "date" en datetime
 data["date"] = pd.to_datetime(data["date"])
--- intelligence-artificielle/f2_reduction.py
+++ intelligence-artificielle/f2_reduction.py
@@ -4,12 +4,11 @@ from f1_preparation_data import data
 # On ne conserve que les colonnes qui nous intéressent
 data = data[
     [
-        "latitude",
-        "longitude",
         "descr_athmo",
         "descr_lum",
         "descr_etat_surf",
         "descr_dispo_secu",
         "descr_grav",
+        "code_departement",
     ]
 ]
